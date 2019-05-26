a95a7bc36525966c76a130a32ec3cd142bbf0ff7
CookiesGirl <50276655+CookiesGirl@users.noreply.github.com>
2019-05-26 20:55:38 +0200

    Update Fight.java

--- ULDVELH/src/application/model/beans/Fight.java
+++ ULDVELH/src/application/model/beans/Fight.java
@@ -1,5 +1,43 @@
 package application.model.beans;
+import java.util.ArrayList;
 
+//constructeur
 public class Fight {
+	private int idFight;
+	private String textFight;
+	private ArrayList<Choice> choices=null;
+	private int id;
+
+	
+public Fight(int id,String text) {
+	this.idFight=id;
+	this.textFight = text;
+}
+public int getId() {
+	return idFight;
+}
+public void setId(int id) {
+	this.idFight = id;
+	}	
+//Nom du combat
+public void setText(String text) {
+	this.textFight = text;
+}
+public String getText() {
+	return textFight;
+}
+
+	// EN COURS //
+// startCombat
+	
+//Fight =
+	
+//On affiche l'ennemi ET les choix possible
+	
+//Si choix de fuir = revenir au chap précédent
+	
+//Sinon combattre = instancie le combat
+	
+//Le plus haut rank commence
 	
 }
